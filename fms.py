c5fddffdc49b6075094476ce9b4f26f3e321e17c
Ansari Adeena Maqsood Alam <adeenaansari142@gmail.com>
2026-09-20 21:43:59 +0530

    v1.1-Add skill

--- fms.py
+++ fms.py
@@ -2,3 +2,9 @@ def register_freelancer(name):
     return "Freelancer " + name + " registered"
 
 print(register_freelancer("Aisha"))
+
+#version 1.1
+def register_freelancer(name, skill):
+    return name + " registered with skill " + skill
+
+print(register_freelancer("Aisha", "Python"))
