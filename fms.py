441d1c74730ff074da5426968884dc4d9921aa42
Ansari Adeena Maqsood Alam <adeenaansari142@gmail.com>
2026-09-20 21:45:19 +0530

    v2.0-Add experience

--- fms.py
+++ fms.py
@@ -8,3 +8,9 @@ def register_freelancer(name, skill):
     return name + " registered with skill " + skill
 
 print(register_freelancer("Aisha", "Python"))
+
+#version2.0
+def register_freelancer(name, skill, experience):
+    return name + " registered with " + skill + " and " + str(experience) + " years experience"
+
+print(register_freelancer("Aisha", "Python", 2))
